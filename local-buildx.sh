f6725dc571903d8dd02fe93b662bc8ee0bd83c5f
Lans <Lansenou.Camara@nlr.nl>
2025-11-09 20:53:56 +0100

    Attempt auto sync

--- local-buildx.sh
+++ local-buildx.sh
@@ -6,7 +6,7 @@ YELLOW='\E[1;33m'
 GREEN='\E[1;32m'
 RESET='\E[0m'
 
-DOCKER_IMAGE="${REGISTRY:-}nginxproxymanager/nginx-full"
+DOCKER_IMAGE="${REGISTRY:-}lansje/nginx-full"
 PLATFORMS=linux/amd64,linux/arm64,linux/arm/7
 
 export OPENRESTY_VERSION=1.27.1.2
